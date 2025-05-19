e0cce745b53e6a4c4b69f4969c8cf0a01dfe1ae6
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-05-19 15:18:17 +0200

    Added questions to the first quiz.

--- intro_quiz/__init__.py
+++ intro_quiz/__init__.py
@@ -11,33 +11,33 @@ class C(BaseConstants):
     FALSE = 'False'
     TRUE_FALSE = [TRUE, FALSE]
     QUESTIONS = [
-        dict(text="This study consists of 10 periods.", choices=[TRUE_FALSE], correct=TRUE),
-        dict(text="Each period consists of a hiring phase and a work phase.", choices=[TRUE_FALSE], correct=TRUE),
+        dict(text="This study consists of 10 periods.", choices=TRUE_FALSE, correct=TRUE),
+        dict(text="Each period consists of a hiring phase and a work phase.", choices=TRUE_FALSE, correct=TRUE),
         dict(text="In the hiring phase of each period, each employer <strong>must</strong> make a contract offer to "
-                  "one worker.", choices=[TRUE_FALSE], correct=FALSE),
+                  "one worker.", choices=TRUE_FALSE, correct=FALSE),
         dict(text="In the hiring phase of each period, workers review the offers they received and "
-                  "<strong>must</strong> accept one offer.", choices=[TRUE_FALSE], correct=FALSE),
+                  "<strong>must</strong> accept one offer.", choices=TRUE_FALSE, correct=FALSE),
         dict(text="If a worker rejects an offer received from an employer in the hiring phase of a period, the worker "
                   "<strong>will</strong> be able to receive another offer <strong>from the same employer in that "
-                  "period</strong>.", choices=[TRUE_FALSE], correct=FALSE),
+                  "period</strong>.", choices=TRUE_FALSE, correct=FALSE),
         dict(text="Employer offers include a salary and whether training is provided during the period. Regardless "
                   "of the worker's effort decision during the subsequent work phase, the salary and training "
-                  "specified in the accepted offer must be provided.", choices=[TRUE_FALSE], correct=TRUE),
+                  "specified in the accepted offer must be provided.", choices=TRUE_FALSE, correct=TRUE),
         dict(text="Employers and workers who do not contract in the hiring phase of a period receive only their "
                   "initial endowment for that period and do not complete the work phase. Employers do not receive "
                   "revenue from worker effort and workers do not receive a salary from an employer.",
-             choices=[TRUE_FALSE], correct=TRUE),
+             choices=TRUE_FALSE, correct=TRUE),
         dict(text="In the work phase of each period, every hired worker must decide how much effort to exert. Higher "
                   "levels of effort are <strong>more costly</strong> to the worker than lower levels of effort.",
-             choices=[TRUE_FALSE], correct=TRUE),
+             choices=TRUE_FALSE, correct=TRUE),
         dict(text="For every worker skill level, the higher the level of effort a worker chooses, the higher an "
-                  "employer's revenue is.", choices=[TRUE_FALSE], correct=TRUE),
+                  "employer's revenue is.", choices=TRUE_FALSE, correct=TRUE),
         dict(text="Worker's productivity is based on his/her skill level. The higher the worker's skill level, the "
                   "higher the worker's productivity is and, thus, the higher the revenue for the employer is for "
-                  "a given effort level.", choices=[TRUE_FALSE], correct=TRUE),
+                  "a given effort level.", choices=TRUE_FALSE, correct=TRUE),
         dict(text="In the hiring phase in <strong>each</strong> of the 10 periods, every employer can again select "
                   "<strong>any</strong> potential worker to make a contract offer to and every worker can freely "
-                  "accept one of the contract offers they receive.", choices=[TRUE_FALSE], correct=TRUE),
+                  "accept one of the contract offers they receive.", choices=TRUE_FALSE, correct=TRUE),
     ]
     NUM_ROUNDS = len(QUESTIONS)
 
@@ -117,8 +117,7 @@ class Quiz1(Page):
 
     @staticmethod
     def vars_for_template(player):
-        q = C.QUESTIONS[player.round_number - 1]
-        return dict(question=q['text'], choices=q['choices'])
+        return dict(questions=C.QUESTIONS)
 
     # @staticmethod
     # def app_after_this_page(player, upcoming_apps):
