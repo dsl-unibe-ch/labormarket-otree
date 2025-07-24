59eda007b372ba0b396a1c6384192cf4760667be
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-07-24 14:34:32 +0200

    Added form validation and submission login for the new offer confirmation popup.

--- _static/labor_market/js/offer_confirmation_popup.js
+++ _static/labor_market/js/offer_confirmation_popup.js
@@ -1,5 +1,9 @@
 function offerConfirmationModalOnConfirm(event) {
-    console.log(event);
+    const modalEl = document.getElementById("offer-confirmation-modal")
+    const modal = bootstrap.Modal.getInstance(modalEl)
+    modal.hide()
+
+    event.target.form.submit();
 }
 
 function showOfferConfirmationModal(worker, salary, training) {
@@ -12,14 +16,17 @@ function showOfferConfirmationModal(worker, salary, training) {
     salaryEl.innerHTML = salary;
     trainingEl.innerHTML = training ? "Yes" : "No";
 
-    // Remove any existing listener
-    modalEl.removeEventListener("hidden.bs.modal", offerConfirmationModalOnConfirm);
-    modalEl.addEventListener("hidden.bs.modal", offerConfirmationModalOnConfirm);
+    const confirmButtonEl = document.getElementById("offer-confirmation-modal-confirm-button");
+    confirmButtonEl.removeEventListener("click", offerConfirmationModalOnConfirm);
+    confirmButtonEl.addEventListener("click", offerConfirmationModalOnConfirm);
 
-    const modal = new bootstrap.Modal(modalEl, {
+    let modal = bootstrap.Modal.getInstance(modalEl);
+    if (!modal) {
+      modal = new bootstrap.Modal(modalEl, {
         backdrop: "static",
         keyboard: true
-    });
+      });
+    }
 
     modal.show();
 }
@@ -27,15 +34,25 @@ function showOfferConfirmationModal(worker, salary, training) {
 function onSendOfferClick(event) {
     event.preventDefault();
 
-    const employeeInput = document.querySelector("input[name='offer_employee']:checked");
-    const salaryInput = document.getElementById("id_offer_wage");
-    const trainingInput = document.querySelector("input[name='offer_training']:checked");
+    const form = event.target.form;
 
-    const worker = employeeInput ? employeeInput.dataset.label : "";
-    const salary = salaryInput ? salaryInput.value : "";
-    const training = trainingInput ? trainingInput.value === "True" : false;
+    if (!form.checkValidity()) {
+      if (form.reportValidity) {
+        form.reportValidity();
+      } else {
+        alert(msg.ieErrorForm);
+      }
+    } else {
+      const employeeInput = document.querySelector("input[name='offer_employee']:checked");
+      const salaryInput = document.getElementById("id_offer_wage");
+      const trainingInput = document.querySelector("input[name='offer_training']:checked");
 
-    showOfferConfirmationModal(worker, salary, training);
+      const worker = employeeInput ? employeeInput.dataset.label : "";
+      const salary = salaryInput ? salaryInput.value : "";
+      const training = trainingInput ? trainingInput.value === "True" : false;
+
+      showOfferConfirmationModal(worker, salary, training);
+    }
 }
 
 const sendOfferInput = document.getElementById("send_offer");
