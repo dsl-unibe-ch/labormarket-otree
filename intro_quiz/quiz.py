0a3d746f8df4e6060ef3d2a0048367f1d1c0b97a
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-09-03 16:15:03 +0200

    Added the information where applicable that the corresponding values are in points.

--- intro_quiz/quiz.py
+++ intro_quiz/quiz.py
@@ -66,12 +66,12 @@ QUIZ_3_QUESTIONS = [
                            "<strong>half of the revenue</strong> based on the hired worker's productivity (determined "
                            "by his/her skill level) and the effort level the worker chooses in this period.",
              choices=TRUE_FALSE, table=None),
-        dict(index=5, text="Assume an employer offers a contract that includes a salary of 500 and does not include "
+        dict(index=5, text="Assume an employer offers a contract that includes a salary of 500 points and does not include "
                            "training. The worker who accepts the contract has a skill level of 5 and chooses "
                            "an effort level of 5. Including the employer's initial endowment of 800 points, "
                            "what are the employer's earnings in this period?",
              choices=[1510, 1210, 855, 710], table="_tables/global/revenue.html"),
-        dict(index=6, text="Assume an employer offers a contract that <strong>includes a salary of 500</strong> "
+        dict(index=6, text="Assume an employer offers a contract that <strong>includes a salary of 500 points</strong> "
                            "and training. The worker who accepts the contract has a <strong>skill level of 5</strong> "
                            "and chooses an <strong>effort level of 5</strong>. "
                            "Including the employer's initial endowment of 800 points, "
@@ -81,7 +81,7 @@ QUIZ_3_QUESTIONS = [
                            "are comprised of the worker's initial endowment plus the salary paid to him/her by his/her "
                            "employer less the costs associated with the chosen level of effort.",
              choices=TRUE_FALSE, table=None),
-        dict(index=8, text="Assume a worker accepts a contract that includes a <strong>salary of 500</strong> and "
+        dict(index=8, text="Assume a worker accepts a contract that includes a <strong>salary of 500 points</strong> and "
                            "<strong>training</strong>. The worker has a skill level of 5 and chooses "
                            "an effort level of 5. Including the worker's initial endowment of 400 points, "
                            "what  would the <strong>worker's</strong> earnings be in this period?",
