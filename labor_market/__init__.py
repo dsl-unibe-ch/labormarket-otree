650baf5b9ded24fbde21b5652545615bf6ac6c49
sukanyanath1 <sukanya.nath@unibe.ch>
2026-02-27 11:01:50 +0100

    OT-6 updated timeout_seconds

--- labor_market/__init__.py
+++ labor_market/__init__.py
@@ -574,7 +574,7 @@ class MakeOffer(Page):
         If player is an agent, it waits for timeout_seconds. Otherwise, it returns None.
         """
         if should_use_agent(player):
-            timeout_seconds = 20
+            timeout_seconds = player.session.config["page_timeout_seconds_agent"]
             return timeout_seconds  # Enough for LLM API calls to complete
         return None
 
@@ -658,7 +658,7 @@ class GetOffers(Page):
         If player is an agent, it waits for timeout_seconds. Otherwise, it returns None.
         """
         if should_use_agent(player):
-            timeout_seconds = player.session.config["page_timeout_seconds"]
+            timeout_seconds = player.session.config["page_timeout_seconds_agent"]
             return timeout_seconds  # Enough for LLM API calls to complete
         return None
 
@@ -818,7 +818,7 @@ class ChooseEffort(Page):
         If player is an agent, it waits for timeout_seconds. Otherwise, it returns None.
         """
         if should_use_agent(player):
-            timeout_seconds = player.session.config["page_timeout_seconds"]
+            timeout_seconds = player.session.config["page_timeout_seconds_agent"]
             return timeout_seconds  # Enough for LLM API calls to complete
         return None
     
